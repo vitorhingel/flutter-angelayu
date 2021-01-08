97b3fcf5f639e9bb70e9b8e8f4f2992dc9e02b5f
Vitor Hingel <vhahingel@hotmail.com>
2021-01-07 22:01:18 -0300

    Finished Dice App

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,8 +18,19 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
-  var leftDiceNumber = 5;
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void updateDicesNumbers() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,19 +38,31 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-            child: FlatButton(
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-              onPressed: () {
-                print('Left button pressed');
-              },
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: FlatButton(
+                padding: EdgeInsets.all(0.0),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+                onPressed: () {
+                  setState(() {
+                    updateDicesNumbers();
+                  });
+                },
+              ),
             ),
           ),
           Expanded(
-            child: FlatButton(
-              child: Image.asset('images/dice2.png'),
-              onPressed: () {
-                print('Rightt button pressed');
-              },
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: FlatButton(
+                padding: EdgeInsets.all(0.0),
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+                onPressed: () {
+                  setState(() {
+                    updateDicesNumbers();
+                  });
+                },
+              ),
             ),
           ),
         ],
